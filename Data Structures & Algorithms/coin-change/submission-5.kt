642a78111d09bbc7e848265276d368f6361f24ac
Amit Vishwakarma <48216000+amitvishdlw@users.noreply.github.com>
2026-03-29 21:56:05 +0530

    Add: coin-change - submission-5

---
+++ Data Structures & Algorithms/coin-change/submission-5.kt
@@ -0,0 +1,29 @@
+class Solution {
+    fun coinChange(coins: IntArray, amount: Int): Int {
+        if (amount < 0) return -1
+        if (amount == 0) return 0
+        coins.sortDescending()
+
+        val dp = IntArray(amount+1) { Int.MAX_VALUE }
+        dp[0] = 0
+        for (i in 0 until coins.size) {
+            if (coins[i] <= amount)
+            dp[coins[i]] = 1
+        }
+
+        for (sum in 1..amount) {
+            for (i in 0 until coins.size) {
+                if (sum >= coins[i] && dp[sum - coins[i]] != Int.MAX_VALUE) {
+                    dp[sum] = minOf(dp[sum], 1 + dp[sum - coins[i]])
+                }
+            }
+        }
+
+        // dp.forEach {
+        //     print("${if (it == Int.MAX_VALUE) -1 else it}, ")
+        // }
+        // println("")
+
+        return if (dp[amount] == Int.MAX_VALUE) -1 else dp[amount]
+    }
+}
